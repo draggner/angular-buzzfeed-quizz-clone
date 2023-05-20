59dfcc6315be25444ff9d973a9dadab78def32cf
Draggner <davisamueldragg@gmail.com>
2023-05-20 18:28:43 -0300

    Etapa de components dinâmicos, concluida.

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  //Variaveis dinâmicas para o quizz.component.html
+  titleQuizz: string = "Titulo Segundario"
+  buttonQuizz: string = "Opções"
+  resultsQuizz: string = "O seu resultado é:"
+  respostQuizz: string = "Resposta Selecionada"
 
+  questions: any
+  questionSelected: any = "Pergunta Selecionada"
+
+  //Armazenar a respostas do user
+    answers: string[] = []
+    answerSelected: string = ""
+
+  //Algoritmo que armaneza a quantidade de perguntas
+    questionIndex: number = 0
+    questionMaxIndex: number = 0
+  
+  //Resultado final do quizz, quando true exibe a Tela de results
+    finished: boolean = false;
+  constructor() { }
+  
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
